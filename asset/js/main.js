256c8096f15b7e2b353d80091a9219a2a28accd5
Taoista <taoista-@hotmail.com>
2024-02-15 05:26:29 -0300

    demo y tutorial

---
+++ asset/js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('#flipbook').turn({
+        width: 3000, // Ancho del flipbook
+        height: 3000, // Altura del flipbook
+        autoCenter: true // Centrar automáticamente el flipbook en la página
+    });
+});
